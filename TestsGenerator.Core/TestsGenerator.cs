acdd9998a995f19fd379217d95120c7aff067c0d
boy-eff <boyev.danila@gmail.com>
2022-10-29 17:07:34 +0300

    Add basic project structure

---
+++ TestsGenerator.Core/TestsGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace TestsGenerator.Core
+{
+    class TestsGenerator
+    {
+        
+    }
+}
+
