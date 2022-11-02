9408f056c789ca0d5179d7eea80e24b9a452639d
boy-eff <boyev.danila@gmail.com>
2022-11-03 00:47:42 +0300

    Add multiple namespaces support

--- TestsGenerator.Example/Program.cs
+++ TestsGenerator.Example/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TestsGenerator.Core;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var files = Directory.GetFiles(@"C:\Users\Asus\Desktop\5 семестр\СПП\4 лаба\TestsGenerator.Example\InputFiles");
+        var generator = new Generator();
+        generator.GenerateAsync(files, @"C:\Users\Asus\Desktop\5 семестр\СПП\4 лаба\TestsGenerator.Example\OutputFiles", 1, 1, 1).Wait();
+    }
+}
